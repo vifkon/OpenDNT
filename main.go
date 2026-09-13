6bceb12d9f11947590e165082497c9a903a2e735
vkkkon <138321853+wiksadeey@users.noreply.github.com>
2026-09-14 00:12:04 +0300

    генерируем ключи, открываем порт. Впрочем ничего интересного

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/flynn/noise"
+)
 
 func main() {
 	fmt.Println("OpenDNT node starting...")
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	cs := noise.NewCipherSuite(noise.DH25519, noise.CipherChaChaPoly, noise.HashSHA256)
+
+	staticKeyPair, err := cs.GenerateKeypair(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Node public key: %x\n", staticKeyPair.Public)
+
+	_ = listener
 }
